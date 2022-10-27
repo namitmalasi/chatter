023bcd88cface1a082fa0a299671c1cbc47c3a9b
Namit Malasi <namitmalasi@gmail.com>
2022-10-27 13:26:10 +0530

    created the search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="find a user" />
+      </div>
 
-export default Search
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/14113987/pexels-photo-14113987.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" />
+        <div className="userChatInfo">
+          <span>Nityansh</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
