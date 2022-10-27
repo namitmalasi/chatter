85a3614a423d2f0df192098176e9b8727e0cab50
Namit Malasi <namitmalasi@gmail.com>
2022-10-27 11:50:53 +0530

    created the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <span className="logo">Chatter</span>
+      <div className="user">
+        <img
+          src="https://images.pexels.com/photos/14113987/pexels-photo-14113987.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <span>Namit</span>
+        <button>logout</button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
